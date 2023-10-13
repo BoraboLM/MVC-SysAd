252bb698c6f4bee1c79395d792532b4310607a3a
Lawrence-kun <borabolm.656.stud@cdd.edu.ph>
2023-10-13 22:58:16 +0800

    fix the url

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,22 +6,19 @@
     <title>Create Task</title>
     
 </head>
-<body class="bg-gray-100 flex justify-center items-center h-screen">
-    <div class="bg-white p-8 rounded shadow-md w-1/2">
-        <h1 class="text-2xl mb-4">Create Task</h1>
-        <a href="{{ route('tasks.index') }}" class="text-blue-500">Back to Task List</a>
-        <form action="{{ route('tasks.store') }}" method="POST" class="mt-4">
-            @csrf
-            <div class="mb-4">
-                <label for="title" class="block text-sm font-medium text-gray-600">Title:</label>
-                <input type="text" name="title" id="title" required class="form-input mt-1 block w-full">
-            </div>
-            <div class="mb-4">
-                <label for="description" class="block text-sm font-medium text-gray-600">Description:</label>
-                <textarea name="description" id="description" rows="3" required class="form-textarea mt-1 block w-full"></textarea>
-            </div>
-            <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Create Task</button>
-        </form>
-    </div>
+<body>
+        <h1>Create Task</h1>
+    <a href="{{ route('tasks.index') }}">Back to Task List</a>
+    <form action="{{ route('tasks.store') }}" method="POST">
+        @csrf
+        <label for="title">Title:</label>
+        <input type="text" name="title" id="title" required>
+        <br>
+        <label for="description">Description:</label>
+        <textarea name="description" id="description" rows="3" required></textarea>
+        <br>
+        <button type="submit">Create Task</button>
+    </form>
+
 </body>
 </html>
